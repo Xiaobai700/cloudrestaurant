053bf085bbf8f387c9737cc16513c7e4114f7c63
zhangqiao <1053373312@qq.com>
2019-09-30 13:41:55 +0800

    Initial commit

---
+++ admin/src/main/java/com/cloud/admin/AdminApplication.java
@@ -0,0 +1,16 @@
+package com.cloud.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangqiao
+ */
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+
+}
---
+++ api/src/main/java/com/cloud/api/ApiApplication.java
@@ -0,0 +1,16 @@
+package com.cloud.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangqiao
+ */
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+}
